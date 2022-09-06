83b5056d827362a13bffc3939f3840369599763a
Steven Jerjian <sjerjian@jhu.edu>
2022-09-06 11:01:09 -0400

    SJ - added 1-targ info to nsEventsConditions

--- NeuroShare/nsEventConditions.m
+++ NeuroShare/nsEventConditions.m
@@ -7,17 +7,15 @@ function [nsEvents] = nsEventConditions(nsEvents,PDS,par)
 % modality are sent as indices in condition list. Here we pull the actual
 % info from PLDAPS
 %
-% SJ 2022
-% 05-2022    fixed issues with mismatched trials
-% 06-14-2022 added 'goodtrial' from PDS, since this isn't in all Ripple
-% files
-% 08-2022    added import of 1-targ behavior from PLDAPS, since this
-% also is not present in Ripple files
+% SJ updates
+% 05-2022 fixed issues with mismatched trials
+% 06-2022 added 'goodtrial' from PDS, since this isn't in all Ripple files
+% 08-2022 added import of 1-targ behavior from PLDAPS, also not sent to Trellis
 
 %% check that unique trial numbers match completely, if not, select NS entries to match
 % note that if multiple PDS files correspond to one Trellis file, this
 % will return just the trials in nsEvents which correspond to the selected
-% PDS file. so probably unwise to overwrite nsEvents in the wrapper!
+% PDS file. i.e. probably unwise to overwrite nsEvents in the wrapper!
 
 for t=1:length(PDS.data)
     PDSutn(t,:) = PDS.data{t}.unique_number;
@@ -35,8 +33,8 @@ matchTrials = ismember(NSutn,PDSutn,'rows');
 
 % 06-2022
 % breakfix vector was 1 entry short in one case, which messes up the
-% matchTrials stuff. let's remove it, and just add in the goodtrial logical
-% later. this needs to be fixed at some point
+% matchTrials stuff. let's just remove it, and just add in the goodtrial logical
+% later. (this needs to be fixed at some point)
 try nsEvents.Events = rmfield(nsEvents.Events,'breakfix'); catch; end
 
 % remove fields only relevant to RFmapping
