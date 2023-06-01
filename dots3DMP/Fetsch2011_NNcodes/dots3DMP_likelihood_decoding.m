36a766f66c7837e7cf3961452f11228d1a1c506b
Yared Haile <76961503+YYboy55@users.noreply.github.com>
2023-06-01 16:19:08 -0400

    tc now generated before loop calculating posterior. This amends use of an undefined nan for d=1 (#38)
    Still need to vectorize 'simChoice' to enable easy psychometric curve fitting

--- dots3DMP/Fetsch2011_NNcodes/dots3DMP_likelihood_decoding.m
+++ dots3DMP/Fetsch2011_NNcodes/dots3DMP_likelihood_decoding.m
@@ -6,48 +6,50 @@ function [tuning_curves, posterior, pop_lh, hdg_intp, simChoice] = dots3DMP_like
 lh_func = @(g,tc,r_obs) (exp(-g*tc) .* bsxfun(@power,g*tc,r_obs)) ./ factorial(r_obs); % vectorized
 
 numunits = length(unique(data.unitnum));
-hdg_intp = min(hdgs):step:max(hdgs);
+hdg_intp = min(hdgs)-4:step:max(hdgs)+4;
 
 % pre-allocate
 tuning_curves = nan(length(mods),length(cohs),length(deltas),length(hdg_intp),numunits);
-r_obs         = nan(length(mods),length(cohs),length(deltas),length(hdgs),numunits,numtrs);
+% r_obs         = nan(length(mods),length(cohs),length(deltas),length(hdgs),numunits,numtrs);
 lh            = nan(length(mods),length(cohs),length(deltas),length(hdgs),length(hdg_intp),numunits,numtrs);
-
 g = 1; % Poisson gain
 
 % surely there's a way to vectorize this...anyway, loops for now
 for m=1:length(mods)
 for c=1:length(cohs)
-for d=1:length(deltas)
 for u=1:numunits
-    x = squeeze(meanFRs(m,c,d,:,u));
-        
+d = 2;
+    x = squeeze(meanFRs(m,c,d,:,u));  
     if any(isnan(x)),continue,end % skip
-
     % linear interpolation
     tuning_curves(m,c,d,:,u) = interp1(hdgs,x,hdg_intp,'linear');
-    
+ 
     % linear regression
 %     p = polyfit(hdgs,x,1);
 %     y = polyval(p,xq);
 %     tuning_curves(m,c,d,:,u) = y;
+end
+end
+end
 
-    % random draws of single-trial firing for each condition
-    % ... and compute likelihood 
-    
-    % tuning curve for given stimulus
+for m=1:length(mods)
+for c=1:length(cohs)
+for d=1:length(deltas)
+for u=1:numunits
+% tuning curve for given stimulus
     tc = squeeze(tuning_curves(m,c,deltas==0,:,u)); % use delta==0!!
-    
-    for h=1:length(hdgs)
-        
+for h=1:length(hdgs)   
+% random draws of single-trial firing for each condition
+% ... and compute likelihood 
         J = data.modality==mods(m) & data.coherence==cohs(c) & data.heading==hdgs(h) & data.delta==deltas(d) & data.unitnum==u;
-        
+        % out of all actual trials sum(J) meet criteria. what was spikeRate
+        % on this subset of trials?
         r_trials = round(data.spikeRates(J));
         if isempty(r_trials), continue, end
         shuf_ind = randi(length(r_trials),1,numtrs);
-
-        r = r_trials(shuf_ind);
-        r_obs(m,c,d,h,u,:) = r;
+        clear r
+        r(:,1) = r_trials(shuf_ind); % Spike rates assigned to 100 simTrials by sampling from actual observed trial spike rates stored in 'r_trials'
+%         r_obs(m,c,d,h,u,:) = r; % Seemingly unused..?
         
         if any(sum(bsxfun(@power,g*tc,r')) == Inf) || any(factorial(r) == Inf)
             %                 disp(['Inf problem! Cell ID #' num2str(n)]);
@@ -70,17 +72,17 @@ for u=1:numunits
         
         lh(m,c,d,h,:,u,:) = lh_func(g,tc,r');
         
-    end
+end
 end
 end
 end
 end
 
 lh(lh==0) = .01;               % kluge to avoid prod(0...)
-pop_lh = squeeze(prod(lh,6));   % population likelihood, product across units
+pop_lh = squeeze(prod(lh,6,'omitnan'));   % population likelihood, product across units
 
 % normalize to get posterior, assuming flat prior
-posterior = pop_lh ./ sum(pop_lh,5);
+posterior = pop_lh ./ sum(pop_lh,5,'omitnan');
 
 %%
 
@@ -88,11 +90,10 @@ posterior = pop_lh ./ sum(pop_lh,5);
 zerohdg = find(hdg_intp==0);
 negSum = squeeze(nansum(posterior(:,:,:,:,1:zerohdg-1,:,:),5));
 posSum = squeeze(nansum(posterior(:,:,:,:,zerohdg+1:end,:,:),5));
-simChoice = posSum > negSum * 2 - 1;
+simChoice = double(posSum>negSum);
 
 % [~,maxpos] = max(posterior,[],5);
 % MAPhdg     = squeeze(hdg_intp(maxpos));
 % simChoice  = sign(MAPhdg);
-
-simChoice(simChoice==0) = sign(randn);
+% simChoice(simChoice==0) = sign(randn);
 
