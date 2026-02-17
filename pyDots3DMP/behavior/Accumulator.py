[FIX] return to using effective scaled time for stim scaling

pyDots3DMP/behavior/Accumulator.py
@@ -82,28 +82,23 @@ class Accumulator:
         urgency: Optional[Union[np.ndarray,float]] = None
         ):
         """
-        Set accumulator drift rates. Optionally add label for each drift.
-        This also adds a mirrored drift rate for the anti-correlated accumulator, and 
-        updates drift rates based on sensitivity and urgency parameters.
+        Set drift rates for anti-correlated accumulators.
         """
 
         if isinstance(drifts, np.ndarray):
             drifts = np.split(drifts, drifts.shape[1], axis=1)
-        
-        # add corresponding negated value for anti-correlated accumulator
-        # also update drift rates based on sensitivity and urgency, if provided
 
         if labels is None:
             labels = list(range(len(drifts)))
-
-        assert len(drifts) == len(labels), "drift rates and provided labels must match in length"
         self.drift_labels = labels
+
+        assert len(drifts) == len(labels), "drift rates and labels \
+            must have the same number of elements"
         
-        for d, drift in enumerate(drifts):
-            drift = drift * np.array([1, -1])
-            drifts_posneg = _urgency_scaling(drift * sensitivity, self.tvec, urgency)
-            # drifts_posneg = np.cumsum(drifts_posneg * self.dt, axis=0)
-            self.drift_rates.append(drifts_posneg)
+        for drift in drifts:
+            drift2 = drift * np.array([1, -1])
+            drift2 = np.broadcast_to(drift2, (self.tvec.size, 2))
+            self.drift_rates.append(drift2)
             
     def cdf(self, use_vectorized: bool = True):
         """calculate cdf at boundaries for each drift rate, returns

pyDots3DMP/behavior/ddm_testing_script.py
@@ -20,37 +20,46 @@ def toc(tstart):
 
 # %% Initialize grid vectors for diffusion particle
 
-# NOTE: going too low on time_vec (like 0.01) can start to introduce some weirdness
-grid_vec = np.arange(-3, 0, 0.005) 
+# NOTE: 
+# going too low on time_vec (like 0.01) can start to introduce some weirdness
+# other values also have sweet spots e.g. bound ~1 and grid_vec -3 --> 0.
+# e.g. try changing the bound to 3...weird stuff happens.
+
+grid_vec = np.arange(-3, 0, 0.01) 
 time_vec = np.arange(0, 2, 0.025)   
 
 acc, vel = get_stim_urgs(time_vec)
 
 # %% demonstrate use of single Accumulator object
 
-drifts = [0, 0.25, 0.5, 1., 2.]
+drifts = [0.25, 0.5, 1., 2.]
 accum = Accumulator(
     grid_vec=grid_vec,
     tvec=time_vec,
-    bound=1,
+    bound=1.5,
 )
-accum.apply_drifts(drifts, labels=drifts) # this "applies" the drifts in [drifts] by creating [tvecx2] anti-correlated accumulators
+# this "applies" the drifts in [drifts] by creating [tvecx2] anti-correlated accumulators
+accum.apply_drifts(drifts, labels=drifts) 
 
+# this computes the cdf (for choice/RT) and pdf/log_odds (for wager) using method of images
 tstart = tic()
-accum.compute_distrs(return_pdf=False, use_vectorized=True) # this computes the cdf (for choice/RT) and pdf (for wager) using method of images
+accum.compute_distrs(return_pdf=True, use_vectorized=True) 
 time_taken = toc(tstart)
 print(f"Accumulator run took {time_taken:.3f} seconds")
-#accum.log_posterior_odds()
+accum.log_posterior_odds()
 accum.plot();
 
-# %%
+# %% Plot simulation of actual decision variable
 
+# dotted lines show underlying accumulator drifts
+# solid lines show simulated anti-correlated accumulators drawn from sampling
+# black line shows the bound
 dec_var, dv_fig = accum.dv(d_ind=1, show=True)
 
 # %%
 
 init_params = {
-    'kmult': [1, 1],        # ves, vis sensitivites
+    'kmult': [0.5, 0.5],        # ves, vis sensitivites
     'bound': [1, 1, 1],         # ves, vis, comb bounds
     'non_dec_time': [0.3],      # non-decision time (secs)
     'wager_thr': [0.5, 0.5, 0.5],     # log odds threshold for high bets
@@ -59,7 +68,7 @@ init_params = {
 
 # generate list of unique conditions
 X = dots3DMP_create_trial_list(
-    hdgs=[-12, -6, -3, 0, 3, 6, 12],
+    hdgs=[-12, -6, -3, 3, 6, 12],
     mods=[1, 2, 3],
     cohs=[0.3, 0.7],
     deltas=[0],
@@ -71,8 +80,8 @@ ddm = SelfMotionDDM(
     grid_vec=grid_vec,
     tvec=time_vec,
     **init_params, 
-    stim_scaling=True,      # scale ves/vis according to acc/vel signals? can also pass in a 2-length tuple of arrays
-    return_wager=False       # whether to compute pdfs and log odds maps, and return wagers
+    stim_scaling=True,  # scale ves/vis according to acc/vel signals?
+    return_wager=True   # whether to compute pdfs and log odds maps, and return wagers
     )
 
 # %%
@@ -80,11 +89,11 @@ ddm = SelfMotionDDM(
 _, preds_model = ddm.predict(X, n_samples=1, cache_accumulators=True)
 
 # %%
-ves_accum = ddm.accumulators_[(1.0, 0.3, 0.0)]
+ves_accum = ddm.accumulators_[('wager', 1.0)]
 ves_accum.plot()
 
 # %%
 # preds_simul = ddm.simulate(X, n_samples=5, sample_dvs=True, seed=1)
-all_data = pd.concat((X, preds_model), axis=1)
-all_data.sort_values(by=['modality', 'coherence', 'heading'])
+# all_data = pd.concat((X, preds_model), axis=1)
+# all_data.sort_values(by=['modality', 'coherence', 'heading'])
 # %%

pyDots3DMP/behavior/moi.py
@@ -259,11 +259,11 @@ def moi_cdf(
     survival_prob = np.ones_like(tvec)
     flux1, flux2 = np.zeros_like(tvec), np.zeros_like(tvec)
 
-    # mu_T = mu * tvec[:,None]
+    mu_T = mu * tvec[:,None]
 
     # integrate drift for particle position in linear time
-    dt = tvec[1] - tvec[0]
-    mu_T = np.cumsum(np.insert(mu[:-1], 0, 0, 0) * dt, axis=0)
+    # dt = tvec[1] - tvec[0]
+    # mu_T = np.cumsum(np.insert(mu[:-1], 0, 0, 0) * dt, axis=0)
 
     # skip the first sample (t starts at 1)
     for t in range(1, len(tvec)):
@@ -296,8 +296,8 @@ def moi_cdf(
             cdf_add1 = mvn_j.cdf(bound1) - cdf_add
             cdf_add2 = mvn_j.cdf(bound2) - cdf_add
 
-            a_j = _weightj(j, mu_t, sigma, sj, s0)
-            # a_j = _weightj(j, mu[t, :].T, sigma, sj, s0)
+            # a_j = _weightj(j, mu_t, sigma, sj, s0)
+            a_j = _weightj(j, mu[t, :].T, sigma, sj, s0)
             cdf_rest += (a_j * cdf_add)
             cdf1 += (a_j * cdf_add1)
             cdf2 += (a_j * cdf_add2)
@@ -344,11 +344,11 @@ def moi_cdf_mvnun(
     low = np.asarray([-np.inf, -np.inf]) # evaluate cdf from -inf to 0 (bound)
     opts = dict(maxpts=None, abseps=1e-5, releps=1e-5)
 
-    # mu_T = mu * tvec[:,None]
+    mu_T = mu * tvec[:,None]
 
     # integrate drift for particle position in linear time
-    dt = tvec[1] - tvec[0]
-    mu_T = np.cumsum(np.insert(mu[:-1], 0, 0, 0) * dt, axis=0)
+    # dt = tvec[1] - tvec[0]
+    # mu_T = np.cumsum(np.insert(mu[:-1], 0, 0, 0) * dt, axis=0)
 
     # skip the first sample (t starts at 1)
     for t in range(1, len(tvec)):
@@ -373,7 +373,6 @@ def moi_cdf_mvnun(
             cdf_add2 = _mvn.mvnun(low, bound2, sj + mu_t, sigma * t_curr, **opts)[0] - cdf_add
 
             a_j = _weightj(j, mu_t, sigma, sj, s0)
-            # a_j = _weightj(j, mu[t, :].T, sigma, sj, s0)
             cdf_rest += (a_j * cdf_add)
             cdf1 += (a_j * cdf_add1)
             cdf2 += (a_j * cdf_add2)
@@ -416,9 +415,10 @@ def moi_cdf_vec(
     sj_all = np.vstack(sj_list)             # (J,2)
     
     # means: (T, J, 2)
-    # mu_t = mu * tvec_safe[:, None]         # (T,2)
-    dt = tvec[1] - tvec[0]
-    mu_t = np.cumsum(np.insert(mu[:-1], 0, 0, 0) * dt, axis=0)
+    mu_t = mu * tvec_safe[:, None]         # (T,2)
+    # dt = tvec[1] - tvec[0]
+    # mu_t = np.cumsum(np.insert(mu[:-1], 0, 0, 0) * dt, axis=0)
+    
     means = mu_t[:, None, :] + sj_all[None, :, :]  # (T,J,2)
 
     # weights: (T, J)
@@ -429,6 +429,7 @@ def moi_cdf_vec(
     weights = signs[None, :] * np.exp(exponent)
 
     sd = np.sqrt(tvec_safe)[:, None]        # (T,1)
+    sd = np.clip(sd, a_min=0.1, a_max=None)
     rho_param = sigma[0, 1]
 
     # standardized limits shape (T,J)

pyDots3DMP/behavior/selfmotionddm.py
@@ -212,7 +212,7 @@ class SelfMotionDDM:
         hdgs, hdg_inds = np.unique(X['heading'], return_inverse=True)
         hdgs = hdgs.astype(float)
 
-        K_SCALE_FACTOR = 5e4
+        K_SCALE_FACTOR = 1e3
         if not self.stim_scaling:
             b_ves, b_vis = np.ones_like(self.tvec), np.ones_like(self.tvec)
         elif isinstance(self.stim_scaling, tuple):
@@ -250,13 +250,14 @@ class SelfMotionDDM:
                 # set accumulators with absolute drifts, for log odds mappings
                 accumulator = Accumulator(grid_vec=self.grid_vec, tvec=self.tvec, bound=bound[m])
 
-                abs_drifts, accumulator.tvec = calc_selfmotion_drifts(
+                abs_drifts, t_eff = calc_selfmotion_drifts(
                     b_vals[m], k_vals_fixed[m], self.tvec, hdgs[hdgs>=0], delta=0,
                     )
+                accumulator.tvec = t_eff
 
                 # run the method of images - diffusion to bound to extract pdfs, cdfs, and LPO
                 accumulator.apply_drifts(abs_drifts, hdgs[hdgs>=0])
-                accumulator.compute_distrs(return_pdf=True) # get the pdfs for wager calculation
+                accumulator.compute_distrs(return_pdf=True, use_vectorized=True) # get the pdfs for wager calculation
 
                 if cache_accumulators:
                     self.accumulators_[('wager', mod)] = accumulator
@@ -288,11 +289,13 @@ class SelfMotionDDM:
                     drifts, t_eff = calc_selfmotion_drifts(
                         b_vals[m], k_vals[m], self.tvec, hdgs, delta=delta,
                         )
+                    accumulator.tvec = t_eff
+                    
                     # this time use signed headings
                     accumulator.apply_drifts(drifts, hdgs) 
 
                     # run the method of images - diffusion to bound to extract pdfs, cdfs, and LPO
-                    accumulator.compute_distrs(return_pdf=self.return_wager, use_vectorized=False)
+                    accumulator.compute_distrs(return_pdf=self.return_wager, use_vectorized=True)
 
                     if cache_accumulators:
                         self.accumulators_[(mod, coh, delta)] = accumulator
@@ -618,11 +621,12 @@ def calc_selfmotion_drifts(
     sin_hdgs = np.sin(np.deg2rad(hdgs))
     dt = np.gradient(tvec)
 
+    cumul_bt = np.cumsum((b_t**2)/(b_t**2).sum(axis=0), axis=0)
+    
     if isinstance(b_k, (int, float)):
         # only one sensitivity and time-course - ves or vis (logic is the same)
-        b_t = b_t.reshape(-1, 1)
-        t_eff = np.cumsum(b_t**2, axis=0) * dt
-        drifts = b_t**2 * b_k * sin_hdgs # see Drugowitsch et al. 2014 supp eq 2 & 7
+        t_eff = cumul_bt * tvec[-1]
+        drifts = np.reshape(b_t, (-1, 1))**2 * b_k * sin_hdgs # see Drugowitsch et al. 2014 supp eq 2 & 7
         # drifts = b_k * sin_uhdgs   # w/o stim scaling, reduces to this
 
     elif len(b_k) == 2:
@@ -642,7 +646,9 @@ def calc_selfmotion_drifts(
         drifts = w_ves * drift_ves + w_vis * drift_vis
 
         # Drugowitsch et al. 2014 supp eq 14
-        t_eff = w_ves**2 * b_t[:,0]**2 + w_vis**2 * b_t[:,1]**2
-        t_eff = np.cumsum(t_eff) * dt
+        t_eff = (w_ves**2 * cumul_bt[:,0] + w_vis**2 * cumul_bt[:,1]) * tvec[-1]
+
+    # cumsum over time, then divide by t_eff to get instantaneous drifts
+    drifts = np.cumsum(drifts, axis=0) / t_eff[:, None]
 
     return drifts, t_eff